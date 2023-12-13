8e19f5cd58e165a2af0856b8b94c62806bc833cd
mvykool <maykolandres4@gmail.com>
2023-12-13 02:24:26 -0500

    created base crud

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  //GET NINJAS
+  @Get()
+  getNinjas() {
+    return [];
+  }
+  //GET NINJAS:ID
+  @Get(':id')
+  getOneNinja() {
+    return [];
+  }
+  //POST NINJAS
+  @Post()
+  createNinja() {
+    return {};
+  }
+  //PUT NINJAS:ID
+  @Put(':id')
+  updateNinja() {
+    return {};
+  }
+  //DELETE NINJAS
+  @Delete(':id')
+  deleteNinja() {
+    return {};
+  }
+}
