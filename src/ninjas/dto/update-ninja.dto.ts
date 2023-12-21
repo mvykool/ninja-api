dae91a11253d6de444d6c2fbaabb2b45e91d4e02
mvykool <maykolandres4@gmail.com>
2023-12-20 21:49:40 -0500

    added new changes

--- src/ninjas/dto/update-ninja.dto.ts
+++ src/ninjas/dto/update-ninja.dto.ts
@@ -1,7 +1,4 @@
 import { CreateNinjaDto } from './create-ninja.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
 export class UpdateNinjaDto extends PartialType(CreateNinjaDto) {}
-function PartialType(CreateNinjaDto: typeof CreateNinjaDto) {
-  throw new Error('Function not implemented.');
-}
-
