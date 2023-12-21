dcab96ce214cfa637c2e1bd6197cc91dbbf285cc
mvykool <maykolandres4@gmail.com>
2023-12-20 21:55:03 -0500

    added update

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -33,9 +34,10 @@ export class NinjasController {
   }
   //PUT NINJAS:ID
   @Put(':id')
-  updateNinja(@Param('id') id: string) {
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
     return {
       id,
+      name: updateNinjaDto,
     };
   }
   //DELETE NINJAS
