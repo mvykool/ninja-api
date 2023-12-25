8d7e36d51dfc438a140c322817555ad7e06cc330
mvykool <maykolandres4@gmail.com>
2023-12-24 21:54:34 -0500

    added validator for weapon

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,8 +1,9 @@
-import { MinLength } from 'class-validator';
+import { IsEnum, MinLength } from 'class-validator';
 
 export class CreateNinjaDto {
   @MinLength(3)
   name: string;
 
+  @IsEnum(['stars', 'katanas'], { message: 'Please use right weapon' })
   weapon: 'stars' | 'katanas';
 }
